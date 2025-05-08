93bb65ab44a674641ff392cb67b9ec497e180ecc
aseelgamal22 <aseelbadr501@gmail.com>
2025-05-09 01:46:16 +0300

    Create Homedata.jsx

---
+++ src/components/Homedata.jsx
@@ -0,0 +1,84 @@
+const shopdata=[
+{ id:1, title:'3d Glass', amount:"(5)"},
+{ id:2, title:"Bluetooth" ,amount: "(1)"},
+  { id:3, title:"Camera", amount: "(6)"},
+    {  id:4 , title:"Drone" , amount:"(2)"},
+    
+    {  id:5 , title:"future product" , amount:"(1)"},
+    {  id:6 , title:"Gamepad" , amount:"(1)"},
+    {  id:7 , title:"google glass" , amount:"(1)"},
+    {  id:8 , title:"headphone" , amount:"(2)"},
+    {  id:9 , title:"laptop" , amount:"(3)"},
+    {  id:10 , title:"LED tv" , amount:"(3)"},
+    {  id:11, title:"light" , amount:"(1)"},
+    {  id:12 , title:"mobile" , amount:"(1)"},
+    {  id:13 , title:"portable" , amount:"(2)"},
+    {  id:1 , title:"projector" , amount:"(1)"},
+    {  id:1 , title:"Samsung" , amount:"(2)"},
+   
+    
+]
+export{shopdata};
+
+const watchdata=[
+    { id:0, category:"test" , title:" 3D Glass", image:"/src/watchh1.jpg " ,price:540 ,image2:"/src/watchh7.jpg" ,quantity:0, btn:"Add To Cart" },
+
+
+
+    { id:1, category:"test1" , title:"3D VR Glass", image:"/src/watchh2.jpg " ,price:256,image2:"/src/watchh1.jpg" ,quantity:0, btn:"Add To Cart" },
+
+
+
+    { id:2, category:"test2" , title:"bluetooth speaker", image:"/src/watchh3.jpg " , price:70 ,image2:"/src/watchh2.jpg" ,quantity:0, btn:"Add To Cart"  },
+    { id:3, category:"test3" , title:"colorful led lights", image:"/src/watchh4.jpg " ,price:"$90.00",image2:"/src/watchh3.jpg" ,quantity:0  , btn:"Add To Cart" },
+
+
+
+    { id:3, category:"test4" , title:"Diamon Wtches", image:"/src/watchh5.jpg "  ,price:210,image2:"/src/watchh4.jpg",quantity:0 , btn:"Add To Cart"  },
+
+
+
+    { id:5, category:"test5" , title:"Drone wifi FPV", image:"/src/watchh6.jpg ", price:235,image2:"/src/watchh5.jpg"  ,quantity:0, btn:"Add To Cart"  },
+
+
+
+    { id:6, category:"test6" , title:"Drone helicopter", image:"/src/watchh7.jpg " ,price:88 ,image2:"/src/watchh6.jpg",quantity:0 , btn:"Add To Cart"  },
+
+
+
+    { id:7, category:"test7" , title:"Fuji film Camera", image:"/src/watchh8.jpg " ,price:500,image2:"/src/watchh7.jpg",quantity:0 , btn:"Add To Cart"   },
+
+
+
+    
+
+
+
+
+
+
+]
+export {watchdata};
+const ratingdata=[
+{title:"LED television"  ,before:"$670 "  ,after:"$596", image:"/src/mini1.jpg"},
+{title:"Micfor iPhone"  ,before:"$300 "  ,after:"$100 ",image:"/src/mini3.jpg"},
+{title:"Xpeed Headset"  ,price:"$20",image:"/src/minni2.jpg" }
+
+
+
+
+
+
+
+
+
+
+]
+export{ratingdata};
+
+
+
+
+
+
+
